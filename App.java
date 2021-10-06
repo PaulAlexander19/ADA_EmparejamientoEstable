976487f9ae89c35b847ce124a5f1ef0fcddc9c44
Paul Alexander <pluquec@unsa.edu.pe>
2021-10-06 10:54:16 -0500

    test 2

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App {
         String[] mens = {"Victor", "William", "xavier", "Yancey", "Zeus"};
         String[] womens = {"Amy", "Bertha", "Claire", "Diane", "Erika"};
         // Test       
+        // 2
         int [][] preferenceMens = {{1,0,3,4,2},{3,1,0,2,4},{1,4,2,3,0},{0,3,2,1,4},{1,3,0,4,2}};
         int [][] preferenceWomens = {{4,0,1,3,2},{2,1,3,0,4},{1,2,3,4,0},{0,4,3,2,1},{3,1,4,2,0}};
         
